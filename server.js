e677f094dcdcfe341c04c75d5557db3dff8852fe
alexandercavin <alexander.cavin2@gmail.com>
2024-12-09 23:25:05 +0700

    added 0.0.0.0

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
-const gamificationRoutes = require("./routes/gamificationRoutes")
+const gamificationRoutes = require("./routes/gamificationRoutes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000/", 
-    credentials: true, 
+    origin: "http://localhost:3000/",
+    credentials: true,
   })
 );
 
-app.use(bodyParser.json({ limit: '50mb' })); 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -26,7 +26,6 @@ app.use("/", userRoutes);
 app.use("/", gamificationRoutes);
 app.use("/auth", authRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
